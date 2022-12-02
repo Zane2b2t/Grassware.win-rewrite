6c6c49aa699f5897321e92913e4fd583506d636f
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-12-02 17:10:52 +0200

    Create QuickBow.java

---
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/QuickBow.java
@@ -0,0 +1,27 @@
+package me.alpha432.oyvey.module.modules.combat;
+
+import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.util.player.InventoryUtil;
+import net.minecraft.init.Items;
+import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
+import net.minecraft.util.math.BlockPos;
+
+
+public class QuickBow extends Module {
+    public QuickBow() {
+        super("QuickBow", Category.COMBAT, "Releases bow at a very high speed");
+    }
+
+    @Override
+    public void onUpdate() {
+        if (nullCheck())
+            return;
+
+        if (InventoryUtil.getHeldItem(Items.BOW) && mc.player.isHandActive() && mc.player.getItemInUseMaxCount() >= 3) {
+            mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, mc.player.getHorizontalFacing()));
+            mc.player.connection.sendPacket(new CPacketPlayerTryUseItem(mc.player.getActiveHand()));
+            mc.player.stopActiveHand();
+        }
+    }
+}
