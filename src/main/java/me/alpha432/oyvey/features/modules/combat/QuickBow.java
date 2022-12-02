d80a93f6045806c4d5f6e79ed768c4c100dfc6f8
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-12-02 17:33:10 +0200

    Update QuickBow.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/QuickBow.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/QuickBow.java
@@ -1,7 +1,7 @@
-package me.alpha432.oyvey.module.modules.combat;
+package me.alpha432.oyvey.features.modules.combat;
 
 import me.alpha432.oyvey.features.modules.Module;
-import me.alpha432.oyvey.util.InventoryUtil;
+import me.alpha432.oyvey.util.InventoryUtil2;
 import net.minecraft.init.Items;
 import net.minecraft.network.play.client.CPacketPlayerDigging;
 import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
@@ -18,7 +18,7 @@ public class QuickBow extends Module {
         if (nullCheck())
             return;
 
-        if (InventoryUtil.getHeldItem(Items.BOW) && mc.player.isHandActive() && mc.player.getItemInUseMaxCount() >= 3) {
+        if (InventoryUtil2.getHeldItem(Items.BOW) && mc.player.isHandActive() && mc.player.getItemInUseMaxCount() >= 3) {
             mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, mc.player.getHorizontalFacing()));
             mc.player.connection.sendPacket(new CPacketPlayerTryUseItem(mc.player.getActiveHand()));
             mc.player.stopActiveHand();
