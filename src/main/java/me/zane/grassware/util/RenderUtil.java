665f031f5bb28665c45114ebff81011c8dedacc2
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-12-11 14:20:10 +0200

    Update RenderUtil.java

--- src/main/java/me/zane/grassware/util/RenderUtil.java
+++ src/main/java/me/zane/grassware/util/RenderUtil.java
@@ -39,25 +39,6 @@ public class RenderUtil implements MC {
         final AxisAlignedBB bb = new AxisAlignedBB(pos);
         outlineShader(bb.minX, bb.minY, bb.minZ, bb.maxX, bb.maxY, bb.maxZ);
     }
-    public static void drawRect(float x, float y, float w, float h, int color) {
-        float alpha = (float) (color >> 24 & 0xFF) / 255.0f;
-        float red = (float) (color >> 16 & 0xFF) / 255.0f;
-        float green = (float) (color >> 8 & 0xFF) / 255.0f;
-        float blue = (float) (color & 0xFF) / 255.0f;
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder bufferbuilder = tessellator.getBuffer();
-        GlStateManager.enableBlend();
-        GlStateManager.disableTexture2D();
-        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR);
-        bufferbuilder.pos(x, h, 0.0).color(red, green, blue, alpha).endVertex();
-        bufferbuilder.pos(w, h, 0.0).color(red, green, blue, alpha).endVertex();
-        bufferbuilder.pos(w, y, 0.0).color(red, green, blue, alpha).endVertex();
-        bufferbuilder.pos(x, y, 0.0).color(red, green, blue, alpha).endVertex();
-        tessellator.draw();
-        GlStateManager.enableTexture2D();
-        GlStateManager.disableBlend();
-    }
 
     public static void outlineShader(final double minX, final double minY, final double minZ, final double maxX, final double maxY, final double maxZ) {
         final AxisAlignedBB bb = new AxisAlignedBB(minX, minY, minZ, maxX, maxY, maxZ).offset(RenderUtil.renderOffset());
