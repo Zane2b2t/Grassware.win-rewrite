e0ecc9290b43ff3c8f99cd3f8f8d6961a51e13b4
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-12-13 01:02:41 +0200

    Update GlintModify.java

--- src/main/java/me/zane/grassware/features/modules/render/GlintModify.java
+++ src/main/java/me/zane/grassware/features/modules/render/GlintModify.java
@@ -33,32 +33,7 @@ public class GlintModify extends Module {
                     ClickGui.Instance.getGradient()[1],
                     Math.max(0.0f, 1.0f - alpha)
             );
-            glPushMatrix();
-            glEnable(GL_BLEND);
-            glDisable(GL_TEXTURE_2D);
-            glDisable(GL_DEPTH_TEST);
-            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-            mc.getRenderManager().renderEntityStatic(entry.getKey(), mc.getRenderPartialTicks(), false);
-            glEnable(GL_DEPTH_TEST);
-            glEnable(GL_TEXTURE_2D);
-            glDisable(GL_BLEND);
-            glPopMatrix();
-
-            glPushMatrix();
-            glEnable(GL_BLEND);
-            glDisable(GL_TEXTURE_2D);
-            glDisable(GL_DEPTH_TEST);
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            glEnable(GL_LINE_SMOOTH);
-            glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
-            glLineWidth(lineWidth.getValue());
-            mc.getRenderManager().renderEntityStatic(entry.getKey(), mc.getRenderPartialTicks(), false);
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-            glEnable(GL_DEPTH_TEST);
-            glEnable(GL_TEXTURE_2D);
-            glDisable(GL_BLEND);
-            glPopMatrix();
-            GradientShader.finish();
+          
         }
     }
 }
