200b39a0c1ef02a7dfde770f82a4327e413c3cd8
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-12-13 17:28:47 +0200

    Create ModuleTools.java

---
+++ src/main/java/me/zane/grassware/features/modules/client/ModuleTools.java
@@ -0,0 +1,33 @@
+package me.zane.grassware.features.modules.client;
+
+import me.zane.grassware.features.modules.Module;
+import me.zane.grassware.features.setting.Setting;
+
+public class ModuleTools extends Module {
+
+    private static ModuleTools INSTANCE;
+
+    public Setting<Notifier> notifier = register(new Setting("ModuleNotifier", Notifier.FUTURE));
+    public Setting<PopNotifier> popNotifier = register(new Setting("PopNotifier", PopNotifier.FUTURE));
+
+
+
+    public static ModuleTools getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new ModuleTools();
+        }
+        return INSTANCE;
+    }
+
+
+    public enum Notifier {
+        TROLLGOD,
+        PHOBOS,
+        FUTURE,
+        DOTGOD,
+        SYNCCLIENT
+    }
+
+
+
+}
