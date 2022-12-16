08a453e6788fc4bec6b8051b9d5bc2acba5900ee
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-12-16 13:40:03 +0200

    Create MixinRenderItem.java

---
+++ src/main/java/me/zane/grassware/mixin/mixins/MixinRenderItem.java
@@ -0,0 +1,31 @@
+
+package me.zane.grassware.mixin.mixins;
+
+import me.zane.grassware.GrassWare;
+import me.zane.grassware.event.events.RenderItemEvent;
+// import me.zane.grassware.features.modules.render.GlintModify;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value={RenderItem.class})
+public class MixinRenderItem {
+    @Shadow
+    private void renderModel(IBakedModel model, int color, ItemStack stack) {
+    }
+
+    @ModifyArg(method={"renderEffect"}, at=@At(value="INVOKE", target="net/minecraft/client/renderer/RenderItem.renderModel(Lnet/minecraft/client/renderer/block/model/IBakedModel;I)V"), index=1)
+    private int renderEffect(int oldValue) {
+    }
+
+}
