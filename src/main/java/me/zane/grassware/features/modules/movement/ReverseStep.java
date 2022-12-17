b3ed26926d4ee32fd471542ca61a99dc53bfd22a
MrBubblegum <103787083+MrBubblegum@users.noreply.github.com>
2022-12-17 14:46:20 +0300

    ReverseStep improvements

--- src/main/java/me/zane/grassware/features/modules/movement/ReverseStep.java
+++ src/main/java/me/zane/grassware/features/modules/movement/ReverseStep.java
@@ -5,11 +5,12 @@ import me.zane.grassware.event.events.TickEvent;
 import me.zane.grassware.features.modules.Module;
 
 public class ReverseStep extends Module {
+private final FloatSetting fallSpeed = register("FallSpeed", 2.0f, 1.0f, 20.0f);
 
     @EventListener
     public void onTick(final TickEvent event) {
-        if (mc.player.onGround) {
-            mc.player.motionY = -2.0f;
+        if (mc.player.onGround && !mc.player.isInWater && !mc.player.isInLava && !mc.player.isOnLadder && !mc.gameSettings.keyBindJump.isKeyDown) {
+            mc.player.motionY = -fallSpeed.getValue();
         }
     }
 }
