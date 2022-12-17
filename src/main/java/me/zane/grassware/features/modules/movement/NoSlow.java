42d32f9f513320f7b75fdb8fcf08383c52918233
MrBubblegum <103787083+MrBubblegum@users.noreply.github.com>
2022-12-17 13:01:55 +0300

    Update NoSlow.java

--- src/main/java/me/zane/grassware/features/modules/movement/NoSlow.java
+++ src/main/java/me/zane/grassware/features/modules/movement/NoSlow.java
@@ -1,11 +1,16 @@
 package me.zane.grassware.features.modules.movement;
 
 import me.zane.grassware.event.bus.EventListener;
+import me.zane.grassware.event.events.PacketEvent;
 import me.zane.grassware.event.events.ItemInputUpdateEvent;
 import me.zane.grassware.features.modules.Module;
+import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.network.play.client.CPacketHeldItemChange;
 import net.minecraft.util.MovementInput;
 
 public class NoSlow extends Module {
+       private final BooleanSetting strict = register("Strict", false);
+       private final BooleanSetting toobee = register("2b2t", false);
 
     @EventListener
     public void onItemInputUpdate(final ItemInputUpdateEvent event) {
@@ -13,6 +18,13 @@ public class NoSlow extends Module {
             final MovementInput movementInput = event.movementInput;
             movementInput.moveForward /= 0.2f;
             movementInput.moveStrafe /= 0.2f;
+        }
+    }
+
+    @SubscribeEvent
+    public void onPacketSend(PacketEvent.Send event) {
+        if (event.getPacket() instanceof CPacketPlayer && toobee.getValue() && mc.player.isHandActive() && !mc.player.isRiding()) {
+            mc.player.connection.sendPacket(new CPacketHeldItemChange(mc.player.inventory.currentItem));
         }
     }
 
