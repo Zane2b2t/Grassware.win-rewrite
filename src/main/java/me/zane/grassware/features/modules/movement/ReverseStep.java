98fd7c65e6b8591635729c40606a0729964fd760
MrBubblegum <103787083+MrBubblegum@users.noreply.github.com>
2022-12-17 16:23:18 +0300

    last fix

--- src/main/java/me/zane/grassware/features/modules/movement/ReverseStep.java
+++ src/main/java/me/zane/grassware/features/modules/movement/ReverseStep.java
@@ -11,11 +11,12 @@ private final FloatSetting fallSpeed = register("FallSpeed", 2.0f, 1.0f, 20.0f);
 
     @EventListener
     public void onTick(final TickEvent event) {
-        if (mc.player.onGround) {
-            mc.player.motionY = -fallSpeed.getValue();
-
         if (mc.player.isElytraFlying() || mc.player.isOnLadder() || mc.player.capabilities.isFlying || mc.player.motionY > 0.0 || mc.gameSettings.keyBindJump.isKeyDown()) {
             return;
+
+        if (mc.player.onGround) {
+            mc.player.motionY = -fallSpeed.getValue();
+            }
         }
     }
 }
