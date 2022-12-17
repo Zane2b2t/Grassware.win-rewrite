53b5ee6402d9d37cd6a10abd2078c5a8c7f19020
MrBubblegum <103787083+MrBubblegum@users.noreply.github.com>
2022-12-17 15:21:14 +0300

    Update ReverseStep.java

--- src/main/java/me/zane/grassware/features/modules/movement/ReverseStep.java
+++ src/main/java/me/zane/grassware/features/modules/movement/ReverseStep.java
@@ -4,14 +4,18 @@ import me.zane.grassware.features.setting.impl.FloatSetting;
 import me.zane.grassware.event.bus.EventListener;
 import me.zane.grassware.event.events.TickEvent;
 import me.zane.grassware.features.modules.Module;
+import net.minecraft.client.settings.KeyBinding;
 
 public class ReverseStep extends Module {
 private final FloatSetting fallSpeed = register("FallSpeed", 2.0f, 1.0f, 20.0f);
 
     @EventListener
     public void onTick(final TickEvent event) {
-        if (!mc.player.isElytraFlying() || !mc.player.isInWater || !mc.player.isInLava || !mc.player.isOnLadder() || !mc.player.capabilities.isFlying || !mc.player.motionY > 0.0 || !mc.gameSettings.keyBindJump.isKeyDown()) {
+        if (!mc.player.isElytraFlying() || !mc.player.isOnLadder() || !mc.player.capabilities.isFlying || !mc.player.motionY > 0.0 || !mc.gameSettings.keyBindJump.isKeyDown()) {
             mc.player.motionY = -fallSpeed.getValue();
+        }
+        if (mc.player.isInWater || mc.player.isInLava) {
+            return;
         }
     }
 }
