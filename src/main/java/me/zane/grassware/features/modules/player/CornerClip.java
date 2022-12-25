18f12443dcd078f9f1266d06bb6bf661495ca570
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-12-25 19:59:28 +0200

    last

--- src/main/java/me/zane/grassware/features/modules/player/CornerClip.java
+++ src/main/java/me/zane/grassware/features/modules/player/CornerClip.java
@@ -1,7 +1,7 @@
 package me.zane.grassware.features.modules.player;
 
 import me.zane.grassware.features.modules.Module;
-import me.zane.grassware.event.events.TickEvent
+import me.zane.grassware.event.events.TickEvent;
 
 import net.minecraft.network.play.client.CPacketPlayer;
 import org.lwjgl.input.Keyboard;
