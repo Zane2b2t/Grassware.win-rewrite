84154ffd20082c027081282c3784858a3b7d98be
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-12-25 20:06:15 +0200

    real finale

--- src/main/java/me/zane/grassware/features/modules/player/CornerClip.java
+++ src/main/java/me/zane/grassware/features/modules/player/CornerClip.java
@@ -2,6 +2,7 @@ package me.zane.grassware.features.modules.player;
 
 import me.zane.grassware.features.modules.Module;
 import me.zane.grassware.event.events.TickEvent;
+import me.zane.grassware.event.bus.EventListener;
 
 import net.minecraft.network.play.client.CPacketPlayer;
 import org.lwjgl.input.Keyboard;
