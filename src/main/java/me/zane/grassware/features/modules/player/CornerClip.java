b85c3ceffd494714b630a780fd140901b49e4c21
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-12-25 19:55:00 +0200

    Update CornerClip.java

--- src/main/java/me/zane/grassware/features/modules/player/CornerClip.java
+++ src/main/java/me/zane/grassware/features/modules/player/CornerClip.java
@@ -1,6 +1,8 @@
 package me.zane.grassware.features.modules.player;
 
 import me.zane.grassware.features.modules.Module;
+import me.zane.grassware.event.events.TickEvent
+
 import net.minecraft.network.play.client.CPacketPlayer;
 import org.lwjgl.input.Keyboard;
 
@@ -9,7 +11,7 @@ public class CornerClip extends Module {
 // made by WMS gaming -ZANE
         }
     @Override
-    public void onTick(); {
+    public void onTick(final TickEvent event) {
         mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX,mc.player.posY - 0.0042123,mc.player.posZ,mc.player.onGround));
         mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX,mc.player.posY - 0.02141,mc.player.posZ,mc.player.onGround));
         mc.player.connection.sendPacket(new CPacketPlayer.PositionRotation(mc.player.posX,mc.player.posY - 0.097421,mc.player.posZ,500,500,mc.player.onGround));
