30e6aace00540b57d2dfa1b2d9e2dd9ec5f2c5f7
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-01-01 12:09:04 +0200

    Update discord rpc to be 0.0.5 instead of 0.0.4

--- src/main/java/me/zane/grassware/discord/Discord.java
+++ src/main/java/me/zane/grassware/discord/Discord.java
@@ -17,9 +17,9 @@ public class Discord {
         discordRPC.Discord_Initialize(discordID, eventHandlers, true, null);
 
         discordRichPresence.startTimestamp = System.currentTimeMillis() / 1000L;
-        discordRichPresence.details ="0.0.4";
+        discordRichPresence.details ="0.0.5";
         discordRichPresence.largeImageKey = "large";
-        discordRichPresence.largeImageText = "Grassware 0.0.4";
+        discordRichPresence.largeImageText = "Grassware 0.0.5";
         discordRichPresence.smallImageKey = "small";
        // discordRichPresence.smallImageText = "AtAt HQ";
         discordRichPresence.state = null;
