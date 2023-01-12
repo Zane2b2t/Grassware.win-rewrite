e0bfe20c1a7843e907004d83621b3ceeba50fe98
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-01-12 19:18:50 +0200

    ;

--- src/main/java/me/zane/grassware/features/modules/render/BlockHighlight.java
+++ src/main/java/me/zane/grassware/features/modules/render/BlockHighlight.java
@@ -12,7 +12,7 @@ public class BlockHighlight extends Module
 
     public static final IntSetting alpha = register("Alpha", 90, 0, 255);
     public static final IntSetting linewidth = register("LineWidth", 1, 0, 5);
-    public static final BooleanSetting depth = register("Depth", true)
+    public static final BooleanSetting depth = register("Depth", true);
 
     public BlockHighlight() {
    //     super("BlockHighlight", Category.RENDER, "Allows you to change how the block highlight behaves.");
