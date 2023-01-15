2c06e19a5e7de7e178991a4537c4eabc2b7e23e6
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-01-15 21:47:05 +0200

    Update Wireframe.java

--- src/main/java/me/alpha432/oyvey/features/modules/render/Wireframe.java
+++ src/main/java/me/alpha432/oyvey/features/modules/render/Wireframe.java
@@ -44,51 +44,9 @@ public class Wireframe
     private void setInstance() {
         INSTANCE = this;
         }
-    }
-  
- @EventListener
-    public void onRender3D(final Render3DEvent event) {
-        if (mc.gameSettings.thirdPersonView != 0) {
-            return;
-        }
-        GradientShader.setup(
-                ClickGui.Instance.step.getValue(),
-                ClickGui.Instance.speed.getValue(),
-                ClickGui.Instance.getGradient()[0],
-                ClickGui.Instance.getGradient()[1],
-                opacity.getValue()
-        );
-        for (final Entity entity : mc.world.loadedEntityList) {
-            if (!entity.equals(mc.player) && entity instanceof EntityPlayer || entity instanceof EntityEnderCrystal) {
-                glPushMatrix();
-                glEnable(GL_BLEND);
-                glDisable(GL_TEXTURE_2D);
-                glDisable(GL_DEPTH_TEST);
-                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-                mc.getRenderManager().renderEntityStatic(entity, mc.getRenderPartialTicks(), false);
-                glEnable(GL_DEPTH_TEST);
-                glEnable(GL_TEXTURE_2D);
-                glDisable(GL_BLEND);
-                glPopMatrix();
 
-                glPushMatrix();
-                glEnable(GL_BLEND);
-                glDisable(GL_TEXTURE_2D);
-                glDisable(GL_DEPTH_TEST);
-                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-                glEnable(GL_LINE_SMOOTH);
-                glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
-                glLineWidth(lineWidth.getValue());
-                mc.getRenderManager().renderEntityStatic(entity, mc.getRenderPartialTicks(), false);
-                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-                glEnable(GL_DEPTH_TEST);
-                glEnable(GL_TEXTURE_2D);
-                glDisable(GL_BLEND);
-                glPopMatrix();
-            }
-        }
-        GradientShader.finish();
-    }
+  
+
 
     @SubscribeEvent
     public void onRenderPlayerEvent(RenderPlayerEvent.Pre event) {
