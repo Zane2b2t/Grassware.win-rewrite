1704c114af2b3f64482331432378f1a0fa2314d9
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-01-21 15:49:25 +0200

    made this from scratch (missing imports)
    gradient itemchams

---
+++ src/main/java/me/zane/grassware/features/modules/render/ItemChams.java
@@ -0,0 +1,45 @@
+package me.zane.grassware.features.modules.render;
+
+import me.zane.grassware.features.modules.client.ClickGui;
+import me.zane.grassware.event.bus.EventListener;
+import me.zane.grassware.shader.impl.GradientShader;
+import me.zane.grassware.features.setting.impl.FloatSetting;
+import me.zane.grassware.features.setting.impl.BooleanSetting;
+import me.zane.grassware.event.events.Render2DEvent;
+import me.zane.grassware.event.events.RenderItemInFirstPersonEvent;
+import me.zane.grassware.event.events.RenderHandEvent;
+import me.zane.grassware.features.modules.Module;
+import me.zane.grassware.mixin.mixins.IEntityRenderer;
+
+import net.minecraft.client.shader.Framebuffer;
+
+
+public class ItemChams extends Module{
+    private final FloatSetting opacity = register("Opacity", 0.5f, 0.1f, 1.0f);
+    private final FloatSetting linewidth = register("LineWidth", 1.0f, 0.0f, 5.0f);
+    private final BooleanSetting yes = register("Yes", false);
+
+
+    @EventListener
+    public void onRender2D(final Render2DEvent event) {
+       if (this.yes.getValue()) {
+           frameBufferShader.startDraw(event.getPartialTicks());
+// my first module made from scratch :D -ZANE 1/19/2022 (intelij so ez wtf)
+
+           ((IEntityRenderer)this.mc.entityRenderer).invokeRenderHand(event.getPartialTicks(), 1);
+           frameBufferShader.stopDraw((float)this.linewidth.getValue(), (float)this.opacity.getValue());
+       }
+        GradientShader.setup(
+                ClickGui.Instance.step.getValue(),
+                ClickGui.Instance.speed.getValue(),
+                ClickGui.Instance.getGradient()[0],
+                ClickGui.Instance.getGradient()[1],
+                opacity.getValue()
+        );
+        GradientShader.finish();
+    }
+    @EventListener
+    public void onHand(final RenderItemInFirstPersonEvent event);
+
+
+} 
