a5ed30e4997609d2369253f5966a0638bd487be2
It's kisman <81470009+kisman2000@users.noreply.github.com>
2023-01-23 18:09:38 +0300

    rewrote itemcharms

--- src/main/java/me/zane/grassware/features/modules/render/ItemChams.java
+++ src/main/java/me/zane/grassware/features/modules/render/ItemChams.java
@@ -1,57 +1,36 @@
 package me.zane.grassware.features.modules.render;
 
-import me.zane.grassware.features.modules.client.ClickGui;
 import me.zane.grassware.event.bus.EventListener;
 import me.zane.grassware.shader.impl.GradientShader;
-import me.zane.grassware.shader.impl.FramebufferShader;
 import me.zane.grassware.features.setting.impl.FloatSetting;
 import me.zane.grassware.features.setting.impl.BooleanSetting;
 import me.zane.grassware.event.events.Render2DEvent;
-import me.zane.grassware.event.events.RenderItemInFirstPersonEvent;
-// import me.zane.grassware.event.events.RenderHandEvent; (we be using the minecraft one)
 import me.zane.grassware.features.modules.Module;
 import me.zane.grassware.mixin.mixins.IEntityRenderer;
 
-import net.minecraft.client.shader.Framebuffer;
-import net.minecraftforge.client.event.RenderHandEvent; //this one
-// import net.minecraft.world.item.ItemStack; 
-
-
+import net.minecraftforge.client.event.RenderHandEvent;
 
 public class ItemChams extends Module{
     private final FloatSetting opacity = register("Opacity", 0.5f, 0.1f, 1.0f);
-    private final FloatSetting linewidth = register("LineWidth", 1.0f, 0.0f, 5.0f);
-    private final BooleanSetting yes = register("Yes", false);
+    
+    private boolean criticalSection = true;
 
 
     @EventListener
     public void onRender2D(final Render2DEvent event) {
-       if (this.yes.getValue()) {
-           framebufferShader.startDraw(event.getPartialTicks());
-// my first module made from scratch :D -ZANE 1/19/2022 (intelij so ez wtf)
-
-            ((IEntityRenderer) mc.entityRenderer).invokeRenderHand(mc.getRenderPartialTicks(), 2);
-           framebufferShader.stopDraw((float)this.linewidth.getValue(), (float)this.opacity.getValue());
-       }
-        GradientShader.setup(
-                ClickGui.Instance.step.getValue(),
-                ClickGui.Instance.speed.getValue(),
-                ClickGui.Instance.getGradient()[0],
-                ClickGui.Instance.getGradient()[1],
-                opacity.getValue()
-        );
+        GradientShader.setup(opacity.getValue());
+        
+        criticalSection = false;
+        ((IEntityRenderer) mc.entityRenderer).invokeRenderHand(mc.getRenderPartialTicks(), 2);
+        criticalSection = true;
+        
         GradientShader.finish();
     }
-    @EventListener
-    public void renderItemInFirstPerson(final RenderItemInFirstPersonEvent event) {
-        // errors should be fixed now
-        }
     
     @EventListener
-     private void renderHandEvent(RenderHandEvent event) {
-         // for some reason cant do ); must do {}
-         }
-        
-     
-
+    private void renderHandEvent(RenderHandEvent event) {
+        if(criticalSection) {
+            event.cancel();
+        }
+    }
 } 
