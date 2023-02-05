654dc43b66e36b7dd556905ffa92b695aefeeda0
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-02-06 00:48:59 +0200

    Update QuranFacts.java

--- src/main/java/me/zane/grassware/features/modules/misc/QuranFacts.java
+++ src/main/java/me/zane/grassware/features/modules/misc/QuranFacts.java
@@ -1,63 +1,195 @@
 package me.zane.grassware.features.modules.misc;
 
-import me.zane.grassware.features.setting.impl.IntSetting;
-import me.zane.grassware.features.setting.impl.BooleanSetting;
-import me.zane.grassware.util.Timer2;
-import me.zane.grassware.event.events.UpdateEvent;
 import me.zane.grassware.features.modules.Module;
+import me.zane.grassware.features.setting.impl.BooleanSetting;
+import me.zane.grassware.features.setting.impl.IntSetting;
+import me.zane.grassware.util.Timer;
 
-import net.minecraft.client.audio.PositionedSoundRecord;
-import net.minecraft.init.SoundEvents;
+import net.minecraft.network.play.client.CPacketChatMessage;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
 
+public class QuranFacts
+extends Module {
+    private final IntSetting delay = register("Seconds Delay", 7, 1, 20);
+    private final BooleanSetting packetMessage = register("Packet Message", false);
+    public Timer timer = new Timer();
+    public int messageCount = 1;
 
-public class QuranFacts extends Module {
-
-    private final IntSetting delay = register("Delay", 4999, 0, 5000);
-
-    private final BooleanSetting sound = register("Sound", true);
-    private final BooleanSetting randomize = register("Randomize", false);
-
-    private final Timer2 delayTimer = new Timer2();
-    private final List<String> facts = new LinkedList<>();
-
-    public QuranFacts() {
-        facts.add("Quran fact #1 : Islam is the second-largest religion in the world");
-        facts.add("Quran fact #2 : The word Islam means submission to the will of the one God.");
-        facts.add("Quran fact #3 : Quran is 1,382 years old");
-        facts.add("Quran fact #4 : Islam is the last official religion");
-        facts.add("Quran fact #5 : Drinking alchol is haram, which would help the usa alot.");
-        facts.add("Quran fact #6 : women wear hijab to to maintain modesty and privacy from unrelated males");
-        
-    }
-
-    private int stage = 0;
 
     @SubscribeEvent
-    public void onUpdate(UpdateEvent event) {
-        if (mc.player == null) {
+    public void onUpdate() {
+        if (this.messageCount == 1 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> 2b2t is the oldest anarchy server in all of minecraft."));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> 2b2t is the oldest anarchy server in all of minecraft.");
+            }
+            ++this.messageCount;
+            this.timer.reset();
             return;
         }
-        if (stage >= facts.size()) {
-            stage = 0;
-        }
-        Random random = new Random();
-        if (delayTimer.hasReached(delay.getValue())) {
-            if (!randomize.getValue()) {
-                mc.player.sendChatMessage(facts.get(stage));
+        if (this.messageCount == 2 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> 2b2t is not only the oldest anarchy server in minecraft, it is also one of the oldest servers in minecraft."));
             } else {
-                mc.player.sendChatMessage(facts.get(random.ints(0, facts.size()).iterator().nextInt()));
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> 2b2t is not only the oldest anarchy server in minecraft, it is also one of the oldest servers in minecraft.");
             }
-            if (sound.getValue()) {
-                mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.AMBIENT_CAVE, 1.0F));
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 3 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> Players come and go on the oldest anarchy server in minecraft. But there is 1 player, who left their mark on the server forever. That player's name, is popbob"));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> Players come and go on the oldest anarchy server in minecraft. But there is 1 player, who left their mark on the server forever. That player's name, is popbob");
             }
-            stage++;
-            delayTimer.reset();
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 4 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> popbob has griefed more bases and monuments on the server than any other player."));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> popbob has griefed more bases and monuments on the server than any other player.");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 5 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> At one point, just seeing popbob was a death sentence"));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> At one point, just seeing popbob was a death sentence");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 6 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> Kingoros is one of the deadliest vanilla pvpers on 2b2t."));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> Kingoros is one of the deadliest vanilla pvpers on 2b2t.");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 7 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> From 2010 - 2018 only 30 players have ever held a 32k weapon. Less than half of these players have ever used a 32k weapon in battle."));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> From 2010 - 2018 only 30 players have ever held a 32k weapon. Less than half of these players have ever used a 32k weapon in battle.");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 8 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> The dragon egg was used on the oldest anarchy server in minecraft to break bedrock."));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> The dragon egg was used on the oldest anarchy server in minecraft to break bedrock.");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 9 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> 2b2t has the longest nether highways ever built in survival minecraft."));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> 2b2t has the longest nether highways ever built in survival minecraft.");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 10 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> Furnaces were used on 2b2t in order to chunkban players."));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> Furnaces were used on 2b2t in order to chunkban players.");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 11 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> Did you know there is an underground fight club on the oldest anarchy server in minecraft?"));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> Did you know there is an underground fight club on the oldest anarchy server in minecraft?");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 12 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> 2b2t players are trying to destroy trees so new players can't get wood easily"));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> 2b2t players are trying to destroy trees so new players can't get wood easily");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 13 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> Many people have been banned from purchasing priority queue on 2b2t"));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> Many people have been banned from purchasing priority queue on 2b2t");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 14 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> Watercubes are being used on 2b2t to protect builds of historical significance."));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> Watercubes are being used on 2b2t to protect builds of historical significance.");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 15 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> itristan's backdoor books are the rarest things on 2b2t"));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> itristan's backdoor books are the rarest things on 2b2t");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 16 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> 2b2t was never a real anarchy server."));
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> 2b2t was never a real anarchy server.");
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
+        }
+        if (this.messageCount == 17 && this.timer.passedS(this.delay.getValue().intValue())) {
+            if (this.packetMessage.getValue().booleanValue()) {
+                QuranFacts.mc.player.connection.sendPacket(new CPacketChatMessage("<AutoFitFag> Thats all for today FitFam, take it easy and if you plan to play, make sure to stay alive out there."));
+                this.messageCount = 0;
+            } else {
+                QuranFacts.mc.player.sendChatMessage("<AutoFitFag> Thats all for today FitFam, take it easy and if you plan to play, make sure to stay alive out there.");
+                this.messageCount = 0;
+            }
+            ++this.messageCount;
+            this.timer.reset();
+            return;
         }
     }
-
 }
