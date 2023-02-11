b930233f0da0740302fbc09ef37a817c47c6c3fb
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-02-11 19:46:06 +0200

    idk how many errors left

--- src/main/java/me/zane/grassware/features/modules/render/ItemChams.java
+++ src/main/java/me/zane/grassware/features/modules/render/ItemChams.java
@@ -12,26 +12,41 @@ import me.zane.grassware.event.events.RenderItemInFirstPersonEvent;
 import me.zane.grassware.features.modules.Module;
 import me.zane.grassware.mixin.mixins.IEntityRenderer;
 
-import net.minecraft.client.shader.Framebuffer;
+// import net.minecraft.client.shader.Framebuffer;
 import net.minecraftforge.client.event.RenderHandEvent; //this one
 // import net.minecraft.world.item.ItemStack; 
-
 
 
 public class ItemChams extends Module{
     private final FloatSetting opacity = register("Opacity", 0.5f, 0.1f, 1.0f);
     private final FloatSetting linewidth = register("LineWidth", 1.0f, 0.0f, 5.0f);
     private final BooleanSetting yes = register("Yes", false);
+    public boolean forceRender = false;
+    
+    @EventListener
+    public void renderItemInFirstPerson(final RenderItemInFirstPersonEvent event) {
+        if (forceRender || !yes.getValue())
+            return;
+        event.setCancelled(true);
+        
+        }
 
 
     @EventListener
     public void onRender2D(final Render2DEvent event) {
        if (this.yes.getValue()) {
-           framebufferShader.startDraw(event.getPartialTicks());
+           FramebufferShader.startDraw(event.getPartialTicks());
+           
+        GlStateManager.enableAlpha();
+        GlStateManager.pushMatrix();
+        GlStateManager.pushAttrib();
 // my first module made from scratch :D -ZANE 1/19/2022 (intelij so ez wtf)
 
             ((IEntityRenderer) mc.entityRenderer).invokeRenderHand(mc.getRenderPartialTicks(), 2);
-           framebufferShader.stopDraw((float)this.linewidth.getValue(), (float)this.opacity.getValue());
+           FramebufferShader.stopDraw((float)this.linewidth.getValue(), (float)this.opacity.getValue());
+           
+        GlStateManager.popMatrix();
+        GlStateManager.popAttrib();
        }
         GradientShader.setup(
                 ClickGui.Instance.step.getValue(),
@@ -42,15 +57,12 @@ public class ItemChams extends Module{
         );
         GradientShader.finish();
     }
-    @EventListener
-    public void renderItemInFirstPerson(final RenderItemInFirstPersonEvent event) {
-        // errors should be fixed now
-        }
+
     
-    @EventListener
-     private void renderHandEvent(RenderHandEvent event) {
-         // for some reason cant do ); must do {}
-         }
+  //  @EventListener
+    // private void renderHandEvent(RenderHandEvent event) {
+       //  // for some reason cant do ); must do {}
+      //   }
         
      
 
