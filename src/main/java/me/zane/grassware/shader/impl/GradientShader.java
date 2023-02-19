6bc3a399c26947f96146ad6a8f48f7c50c362111
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-02-19 14:44:41 +0200

    Update GradientShader.java

--- src/main/java/me/zane/grassware/shader/impl/GradientShader.java
+++ src/main/java/me/zane/grassware/shader/impl/GradientShader.java
@@ -15,32 +15,28 @@ public class GradientShader implements MC {
     private final static ShaderUtil shader = new ShaderUtil("/assets/minecraft/textures/shaders/gradient.frag");
     private static Framebuffer framebuffer = new Framebuffer(1, 1, false);
 
-    public static void setupUniforms(final float step, final float speed, final Color color, final Color color2, final Color color3, final float opacity) {
+    public static void setupUniforms(final float step, final float speed, final Color color, final Color color2, final float opacity) {
         shader.setUniformi("texture", 0);
         shader.setUniformf("rgb", color.getRed() / 255.0f, color.getGreen() / 255.0f, color.getBlue() / 255.0f);
         shader.setUniformf("rgb1", color2.getRed() / 255.0f, color2.getGreen() / 255.0f, color2.getBlue() / 255.0f);
-        shader.setUniformf("rgb2", color3.getRed() / 255.0f, color3.getGreen() / 255.0f, color3.getBlue() / 255.0f);
         shader.setUniformf("step", 300 * step);
         shader.setUniformf("offset", (float) ((((double) System.currentTimeMillis() * (double) speed) % (mc.displayWidth * mc.displayHeight)) / 10.0f));
         shader.setUniformf("mix", opacity);
     }
 
     public static void setup() {
-        setup(ClickGui.Instance.step.getValue(), ClickGui.Instance.speed.getValue(), ClickGui.Instance.getGradient()[0], ClickGui.Instance.getGradient()[1], ClickGui.Instance.getGradient()[2]);
+        setup(ClickGui.Instance.step.getValue(), ClickGui.Instance.speed.getValue(), ClickGui.Instance.getGradient()[0], ClickGui.Instance.getGradient()[1]);
     }
 
     public static void setup(final float opacity) {
-        setup(ClickGui.Instance.step.getValue(), ClickGui.Instance.speed.getValue(), ClickGui.Instance.getGradient()[0], ClickGui.Instance.getGradient()[1], ClickGui.Instance.getGradient()[2], opacity);
-    }
-    public static void setup(final float step, final float speed, final Color color, final Color color2, final Float opacity) {
-        setup(step, speed, color, color2, opacity);
+        setup(ClickGui.Instance.step.getValue(), ClickGui.Instance.speed.getValue(), ClickGui.Instance.getGradient()[0], ClickGui.Instance.getGradient()[1], opacity);
     }
 
-    public static void setup(final float step, final float speed, final Color color, final Color color2, final Color color3) {
-        setup(step, speed, color, color2, color3, 1.0f);
+    public static void setup(final float step, final float speed, final Color color, final Color color2) {
+        setup(step, speed, color, color2, 1.0f);
     }
 
-    public static void setup(final float step, final float speed, final Color color, final Color color2, final Color color3, final float opacity) {
+    public static void setup(final float step, final float speed, final Color color, final Color color2, final float opacity) {
         GlStateManager.enableBlend();
         GlStateManager.color(1, 1, 1, 1);
         GlStateManager.tryBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO);
@@ -49,7 +45,7 @@ public class GradientShader implements MC {
 
         mc.getFramebuffer().bindFramebuffer(true);
         shader.init();
-        setupUniforms(step, speed, color, color2, color3, opacity);
+        setupUniforms(step, speed, color, color2, opacity);
 
         glBindTexture(GL_TEXTURE_2D, framebuffer.framebufferTexture);
     }
