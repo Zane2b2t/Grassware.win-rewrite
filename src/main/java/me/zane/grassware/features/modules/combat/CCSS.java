bafecf3f4a6592ca61a2a4f303513da4a07495c5
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-02-21 14:35:56 +0200

    improvments to CCSS

--- src/main/java/me/zane/grassware/features/modules/combat/CCSS.java
+++ src/main/java/me/zane/grassware/features/modules/combat/CCSS.java
@@ -1,10 +1,14 @@
 package me.zane.grassware.features.modules.combat;
 
 import me.zane.grassware.features.modules.Module;
+import me.zane.grassware.event.events.UpdateEvent;
 
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.network.play.client.CPacketChatMessage;
+
 
 
 public class CCSS extends Module {
@@ -14,16 +18,19 @@ public class CCSS extends Module {
 // you can silent switch on cc but your ca will be slower, with that you can stil offhand but also silent switch only when there's totem in ur offhand (youre low health you cant offhand rn)
   // so this gives u some extra slow crystals which is better than nothing!
   
-    @Override
-    public void onUpdate() {
+    @SubscribeEvent
+    public void onUpdate(final UpdateEvent event) {
         if (mc.player.getHeldItemOffhand().getItem() == TOTEM_OF_UNDYING) {    // makes autocrystal switch to silent when there's totem in offhand
-            mc.player.sendChatMessage(".AutoCrystal switch Silent");
-            silentMode = true;
+            Util.mc.player.connection.sendPacket(new CPacketChatMessage(".AutoCrystal switch Silent "));
+          Command.sendMessage(ChatFormatting.BOLD + "Silent switch is on!");
+            silentMode = true; 
         }
 
         if (mc.player.getHeldItemOffhand().getItem() == Items.END_CRYSTAL) {  // makes autocrystal switch to none when there's crystal in offhand
-            mc.player.sendChatMessage(".AutoCrystal switch None");
+            Util.mc.player.connection.sendPacket(new CPacketChatMessage("AutoCrystal switch None"));
+          Command.sendMessage(ChatFormatting.BOLD + "Silent switch is off.");
             silentMode = false;
+          return;
         }
 
 
