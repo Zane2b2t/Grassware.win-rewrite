9937d48ac8ba21ae6a6cdbd67ca418b7438c198e
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2023-03-01 11:57:18 +0200

    add xp no delay

---
+++ src/main/java/me/zane/grassware/features/modules/player/XP.java
@@ -0,0 +1,29 @@
+package me.zane.grassware.features.module.modules.player;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemExpBottle;
+import net.minecraft.item.ItemStack;
+
+import me.zane.grassware.features.module.Module;
+import me.zane.grassware.mixin.mixins.IMinecraft;
+
+public class XP extends Module {
+  
+
+    @Override
+    public void init() {
+        this.setModuleStack(new ItemStack(Items.EXPERIENCE_BOTTLE));
+    }
+
+    @Override
+    public void onFastTick() {
+        if (mc.player.getHeldItemMainhand().getItem() instanceof ItemExpBottle
+        || mc.player.getHeldItemOffhand().getItem() instanceof ItemExpBottle) {
+            if(((IMinecraft) mc).getRightClickDelayTimer() != 0) {
+                ((IMinecraft) mc).setRightClickDelayTimer(0);
+            }
+        }
+    }
+}
